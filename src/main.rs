8c3d31dbad27c27e896036b26981f9866fb87e22
kotobukid <kotobuki@daqua.jp>
2021-09-03 00:11:38 +0900

    列挙体の値を簡単にprintln!で出力する例(1)

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,21 @@ fn main() {
     };
 
     println!("{} / {}", a.cat, a.literal);
+    println!("{:?}", token::Cat::ILLEGAL);
+    // println!("{}", token::Cat::EOF);
+    // println!("{}", token::Cat::IDENT);
+    // println!("{}", token::Cat::INT);
+    // println!("{}", token::Cat::ASSIGN);
+    // println!("{}", token::Cat::PLUS);
+    // println!("{}", token::Cat::COMMA);
+    // println!("{}", token::Cat::SEMICOLON);
+    // println!("{}", token::Cat::LPAREN);
+    // println!("{}", token::Cat::RPAREN);
+    // println!("{}", token::Cat::LBRACE);
+    // println!("{}", token::Cat::RBRACE);
+    // println!("{}", token::Cat::FUNCTION);
+    // println!("{}", token::Cat::LET);
     println!("{}", token::sum_in_module(100, 200))
-}
-
-fn sum(a: i32, b: i32) -> i32 {
-    return a + b;
 }
 
 #[cfg(test)]
@@ -19,11 +29,6 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn sum() {
-        assert_eq!(100 + 200, super::sum(100, 200))
     }
 
     #[test]
--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,7 @@
 // mod宣言をしなくてもファイルがモジュールになる模様
+use std::fmt;
+use std::fmt::Formatter;
+
 
 pub fn sum_in_module(a: i32, b: i32) -> i32 {
     return a + b;
@@ -10,3 +13,32 @@ pub struct Token {
     pub cat: TokenType,
     pub literal: String,
 }
+
+#[derive(Debug)]
+pub enum Cat {
+    ILLEGAL,
+    EOF,
+
+    IDENT,
+    INT,
+
+    ASSIGN,
+    PLUS,
+
+    COMMA,
+    SEMICOLON,
+
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+
+    FUNCTION,
+    LET,
+}
+
+// impl fmt::Display for Cat {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+//         write!(f, "{}", self)
+//     }
+// }
