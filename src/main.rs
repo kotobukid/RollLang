3b7129e0481a90d9818b22817659914c91129ff7
kotobukid <kotobuki@daqua.jp>
2021-09-02 23:22:21 +0900

    同一ファイル内モジュールの関数を使用する記法　とテスト

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn sum(a: i32, b: i32) -> i32 {
+    return a + b;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn sum() {
+        assert_eq!(100 + 200, super::sum(100, 200))
+    }
+}
